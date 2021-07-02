739ac964e39f3486838833683ca9799f3b7345a6
Ordinall <abdullah4abidansari@gmail.com>
2021-07-03 03:32:39 +0500

    converted indentation to tabs

--- Plugins/SpotifyListenAlong/SpotifyListenAlong.plugin.js
+++ Plugins/SpotifyListenAlong/SpotifyListenAlong.plugin.js
@@ -12,15 +12,15 @@ module.exports = (_ => {
 	const config = {
 		"info": {
 			"name": "SpotifyListenAlong",
-        	"authors": [{
-            	"name": "ordinall",
-            	"discord_id": "374663636347650049",
-            	"github_username": "ordinall",
-        	}],
-        	"version": "1.0.0",
-        	"description": "Enables Spotify Listen Along feature on Discord without Premium",
-        	"github": "https://github.com/ordinall/BetterDiscord-Stuff/tree/master/Plugins/SpotifyListenAlong/",
-        	"github_raw": "https://raw.githubusercontent.com/ordinall/BetterDiscord-Stuff/master/Plugins/SpotifyListenAlong/SpotifyListenAlong.plugin.js"
+			"authors": [{
+				"name": "ordinall",
+				"discord_id": "374663636347650049",
+				"github_username": "ordinall",
+			}],
+			"version": "1.0.0",
+			"description": "Enables Spotify Listen Along feature on Discord without Premium",
+			"github": "https://github.com/ordinall/BetterDiscord-Stuff/tree/master/Plugins/SpotifyListenAlong/",
+			"github_raw": "https://raw.githubusercontent.com/ordinall/BetterDiscord-Stuff/master/Plugins/SpotifyListenAlong/SpotifyListenAlong.plugin.js"
 		},
 		"changelog": [
 			{
@@ -34,37 +34,37 @@ module.exports = (_ => {
 		"main": "index.js"
 	};
 	return !global.ZeresPluginLibrary ? class {
-        constructor() {this._config = config;}
-        getName() {return config.info.name;}
-        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
-        getDescription() {return config.info.description;}
-        getVersion() {return config.info.version;}
-        load() {
-            const title = "Library Missing";
-            const ModalStack = BdApi.findModuleByProps("push", "update", "pop", "popWithKey");
-            const TextElement = BdApi.findModuleByProps("Sizes", "Weights");
-            const ConfirmationModal = BdApi.findModule(m => m.defaultProps && m.key && m.key() == "confirm-modal");
-            if (!ModalStack || !ConfirmationModal || !TextElement) return BdApi.alert(title, `The library plugin needed for ${config.info.name} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
-            ModalStack.push(function(props) {
-                return BdApi.React.createElement(ConfirmationModal, Object.assign({
-                    header: title,
-                    children: [BdApi.React.createElement(TextElement, {color: TextElement.Colors.PRIMARY, children: [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`]})],
-                    red: false,
-                    confirmText: "Download Now",
-                    cancelText: "Cancel",
-                    onConfirm: () => {
-                        require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-                            if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                            await new Promise(r => require("fs").writeFile(require("path").join(ContentManager.pluginsFolder, "0PluginLibrary.plugin.js"), body, r));
-                        });
-                    }
-                }, props));
-            });
-        }
-        start() {}
-        stop() {}
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Library) => {
+		constructor() {this._config = config;}
+		getName() {return config.info.name;}
+		getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+		getDescription() {return config.info.description;}
+		getVersion() {return config.info.version;}
+		load() {
+			const title = "Library Missing";
+			const ModalStack = BdApi.findModuleByProps("push", "update", "pop", "popWithKey");
+			const TextElement = BdApi.findModuleByProps("Sizes", "Weights");
+			const ConfirmationModal = BdApi.findModule(m => m.defaultProps && m.key && m.key() == "confirm-modal");
+			if (!ModalStack || !ConfirmationModal || !TextElement) return BdApi.alert(title, `The library plugin needed for ${config.info.name} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
+			ModalStack.push(function(props) {
+				return BdApi.React.createElement(ConfirmationModal, Object.assign({
+					header: title,
+					children: [BdApi.React.createElement(TextElement, {color: TextElement.Colors.PRIMARY, children: [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`]})],
+					red: false,
+					confirmText: "Download Now",
+					cancelText: "Cancel",
+					onConfirm: () => {
+						require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+							if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+							await new Promise(r => require("fs").writeFile(require("path").join(ContentManager.pluginsFolder, "0PluginLibrary.plugin.js"), body, r));
+						});
+					}
+				}, props));
+			});
+		}
+		start() {}
+		stop() {}
+	} : (([Plugin, Api]) => {
+		const plugin = (Plugin, Library) => {
 			const { DiscordModules, Patcher } = Library;
 			return class SpotifyListenAlong extends Plugin {
 				constructor() {
@@ -89,5 +89,5 @@ module.exports = (_ => {
 			};
 		};
 		return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
+	})(global.ZeresPluginLibrary.buildPlugin(config));
 })();
