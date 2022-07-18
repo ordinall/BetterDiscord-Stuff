c81b7baca239f7261aced211c8061c2f5c505e40
Abdullah Abid Ansari <abdullah4abidansari@gmail.com>
2022-07-18 19:01:21 +0500

    commit to trigger betterdiscord's webhook

--- Plugins/DisableStickerSuggestions/DisableStickerSuggestions.plugin.js
+++ Plugins/DisableStickerSuggestions/DisableStickerSuggestions.plugin.js
@@ -148,7 +148,7 @@ module.exports = (_ => {
 						this.expressionModule.ExpressionSuggestionsEnabled.updateSetting(!this.expressionModule.ExpressionSuggestionsEnabled.getSetting());
 					}
 				}
-
+ 
 				removePatches() {
 					this.removeEmojiPatch();
 				}
